f3db0db4c3ab316895708ad5666e6f4519cda53e
edward <edwardlfish@gmail.com>
2026-07-24 09:36:21 +0300

    Refactor text model: Optimize sentence splitting by replacing `split` with `re.split`

--- 04_hw_text_processing/text_model.py
+++ 04_hw_text_processing/text_model.py
@@ -1,12 +1,13 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy
+import re
 
 
 class TextModel:
     def __init__(self, text: str):
-        text_arr = text.split('.')
-        self.text_model = [res for s in text_arr if (res := s.strip().rstrip('.!?…'))]
+        text_arr = re.split(r'[.!?…]', text)
+        self.text_model = [res for s in text_arr if (res := s.strip())]
         self.vectorizer = TfidfVectorizer(stop_words="english")
         self.tfidf_text = self.vectorizer.fit_transform(self.text_model)
 
